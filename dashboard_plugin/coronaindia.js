58a089278d4a4acbae3c503aa29908800ffa7e15
ijayasurya <67519756+ijayasurya@users.noreply.github.com>
2020-10-27 10:35:20 -0700

    Update coronaindia.js

--- dashboard_plugin/coronaindia.js
+++ dashboard_plugin/coronaindia.js
@@ -12,7 +12,7 @@ if(window.jQuery)
 {
 	$(function() {
 		
-		var baseUrl = "https://elcomrades.github.io/coronaindia/"
+		var baseUrl = "https://coronavirus.isaiminiweb.online/"
 		var template = unescape("<div id=\"twister\"> <button type=\"button\" class=\"crosscloseif\" onclick=\"hide()\">"
 									+"<svg version=\"1.1\" viewBox=\"0 0 100 100\" xml:space=\"preserve\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" height=\"20\"> <polygon fill=\"#010101\" points=\"77.6,21.1 49.6,49.2 21.5,21.1 19.6,23 47.6,51.1 19.6,79.2 21.5,81.1 49.6,53 77.6,81.1 79.6,79.2 51.5,51.1 79.6,23 \"><\/polygon> <\/svg>"
 									+"<\/button> <div class=\"draggablearea\" > "
